acecec0e852798b6c6e63c4fa99b1d5d55f75eab
Your Name <you@example.com>
2022-03-30 12:58:27 +0200

    login user

--- templates/javaScript/login.js
+++ templates/javaScript/login.js
@@ -1,10 +1,35 @@
-/**
- * 
- * 
- * @param {*} formElement 
- * @param {*} type 
- * @param {*} message 
- */
+let user1 = {
+    'username': "Simon",
+    'password': 5555
+};
+
+let user2 = {
+    'username': "Hoernchen",
+    'password': 6789
+};
+
+let user3 = {
+    'username': "DonSingh",
+    'password': 1234
+};
+
+function checkUser() {
+    let username = username_login.value
+    let password = password_login.value;
+
+    if ((username == user1.username && password == user1.password) || 
+    (username == user2.username && password == user2.password) || 
+    (username == user3.username && password == user3.password)) 
+    {
+        window.location.href = "/templates/html/board.html";
+    }
+    else {
+        alert('Username or password is incorrect');
+    }
+}
+
+
+/* 
 function setFormMessage(formElement, type, message) {
     const messageElement = formElement.querySelector(".form_message");
 
@@ -23,46 +48,25 @@ function clearInputError(inputElement) {
     inputElement.parentElement.querySelector(".form_input_error_message").textContent = "";
 }
 
-/**
- * 
- */
-<<<<<<< HEAD
+
 document.addEventListener("DOMContentLoaded", () => {  // The DOMContentLoaded event is dispatched when the initial HTML document is fully loaded and parsed.
     const loginForm = document.querySelector("#login");  // Constant variable "loginForm" is initialized.
     const createAccountForm = document.querySelector("#create_account");  // Constant variable "createAccountForm" is initialized.
-=======
-document.addEventListener("DOMContentLoaded", () => { // The DOMContentLoaded event is dispatched when the initial HTML document is fully loaded and parsed.
-    const loginForm = document.querySelector("#login"); // Constant variable "loginForm" is initialized
-    const createAccountForm = document.querySelector("#create_account"); // Constant variable "createAccountForm" is initialized
->>>>>>> a522f5e101bf8126cec1a6b577377c373c31714c
 
-    /**
-     * Clicking link_create_account takes you to the registration page.
-     */
+    // Clicking link_create_account takes you to the registration page.
     document.querySelector("#link_create_account").addEventListener("click", e => {
         e.preventDefault();
         loginForm.classList.add("form_hidden"); // login page is hidden.
-<<<<<<< HEAD
         createAccountForm.classList.remove("form_hidden");  // Registration page becomes visible.
-=======
-        createAccountForm.classList.remove("form_hidden"); // Registration page becomes visible
->>>>>>> a522f5e101bf8126cec1a6b577377c373c31714c
     });
 
-    /**
-     * Clicking link_login takes you to the login page.
-     */
+    //Clicking link_login takes you to the login page.
     document.querySelector("#link_login").addEventListener("click", e => {
         e.preventDefault(); // Javascript Event preventDefault does the job of calling return false; in the DOM to override the browser's original action on an HTML element.
         loginForm.classList.remove("form_hidden"); // Login page becoms visible.
         createAccountForm.classList.add("form_hidden"); // Registration page is hidden.
     });
 
-    loginForm.addEventListener("submit", e => {
-        e.preventDefault();
-
-        setFormMessage(loginForm, "error", "Invalid username/password combination");
-    });
 
     document.querySelectorAll(".form_input").forEach(inputElement => {
         inputElement.addEventListener("blur", e => {
@@ -80,21 +84,45 @@ document.addEventListener("DOMContentLoaded", () => { // The DOMContentLoaded ev
 
 // Section to save the users.
 
+async function loadUser() {
+    await downloadFromServer();
+    user = JSON.parse(backend.getItem('user')) || [];
+
+}
+
+
 async function addUser() {
     let username = document.getElementById('signup_username');
     let email = document.getElementById('signup_email');
     let password = document.getElementById('signup_password');
     let confirm_password = document.getElementById('signup_confirm_password');
 
-    if (password  == confirm_password) {
+    if (password.value  == confirm_password.value) {
         let newUser = {
         'id': new Date().getTime(),
         'username': username.value,
         'e-mail_address': email.value,
         'password': password.value,
-        }
+        };
+        
+        username.push(newUser);
+        clearuserInput();
+        await saveUser();
     }
     else {
-        alert('"Password" and "Confirm Password" does not match');
+        alert('"Password" and "Confirm Password" does not match.');
     }
+
 }
+
+function clearuserInput() {
+    document.getElementById('signup_username').value = ``;
+    document.getElementById('signup_email').value = ``;
+    document.getElementById('signup_password').value = ``;
+    document.getElementById('signup_confirm_password').value = ``;
+}
+
+async function saveUser() {
+    await backend.setItem('user', JSON.stringify(user));
+};
+*/
